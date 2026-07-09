55b5e05c52ac1549e152a019d543e28b7db2429f
VuHaiDuong <155283980+WickerBottomJeans@users.noreply.github.com>
2026-07-09 21:31:02 +0700

    FPS set to 60

--- Assets/Scripts/Flow/BootFlow.cs
+++ Assets/Scripts/Flow/BootFlow.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using DefaultNamespace.UI;
+using UnityEngine;
 
 namespace DefaultNamespace
 {
@@ -7,11 +8,19 @@ namespace DefaultNamespace
     {
         public async Task Enter()
         {
+            ConfigureFrameRate();
             await SpriteLoader.Instance.LoadAll();
 
 #if DEVELOPMENT_BUILD || UNITY_EDITOR
             UIManager.Instance.ShowTesterToggle();
 #endif
+        }
+
+        private static void ConfigureFrameRate()
+        {
+            // TODO: This is just temporary so i can test some stuff. MUST redo later
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = 60;
         }
     }
 }
